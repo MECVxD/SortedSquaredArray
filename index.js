3d09f837e3e8562bb93f870e87a6129fcffefbd2
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-03-18 23:59:21 -0600

    code was added with resolution

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function sortedSquaredArray(array) {
   // Write your code here.
-  return [];
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    let aux = 0;
+    aux = array[i] * array[i];
+    result.push(aux);
+  }
+  return result;
 }
 
 // Do not edit the line below.
 exports.sortedSquaredArray = sortedSquaredArray;
+console.log(sortedSquaredArray([1, 2, 3, 5, 6, 8, 9]));
