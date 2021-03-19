492f858a7da36ae4f43ab5f0b57046543cf9fae0
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-03-19 00:22:52 -0600

    code optimized

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ function sortedSquaredArray(array) {
   // Write your code here.
   let result = [];
   for (let i = 0; i < array.length; i++) {
-    let aux = 0;
-    aux = array[i] * array[i];
-    result.push(aux);
+    result[i] = array[i] * array[i];
   }
   return result;
 }
